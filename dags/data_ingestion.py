a0f0d386d49f7dcb17c30dee0bbe7890b8d1f6c3
friday <shvets7200@gmail.com>
2023-09-07 12:05:25 +0000

    add URL template and task

--- dags/data_ingestion.py
+++ dags/data_ingestion.py
@@ -6,20 +6,30 @@ from airflow.operators.python import PythonOperator
 
 AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow")
 
+url = 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-01.parquet'
+URL_PREFIX = "https://d37ci6vzurychx.cloudfront.net/trip-data"
+URL_TEMPLATE = URL_PREFIX + "yellow_tripdata_{{ execution_date.strftime(\'%Y-%m\') }}.parquet"
+OPTPUT_TEMPLATE = AIRFLOW_HOME + "/output_{{ execution_date.strftime(\'%Y-%m\') }}.parquet"
+TABLE_TEMPLATE = "yellow_taxi_{{ execution_date.strftime(\'%Y-%m\') }}"
+
+
 workflow = DAG(
     dag_id='IngestionDag',
     start_date=datetime(2023,1,1),
-    schedule_interval='0 6 2 * *'
+    schedule='0 6 2 * *'
 )
 
 with workflow:
     
     curl_task = BashOperator(
         task_id='curl',
-        bash_command='echo "Hello world"'
+        # bash_command=f'curl -sSL {url} > output.parquet'
+        bash_command='echo " {{ ds }} {{ execution_date.strftime(\'%Y-%m\') }}"'
     )
     
     load_task = BashOperator(
         task_id='load',
         bash_command='echo'
     )
+    
+curl_task >> load_task
